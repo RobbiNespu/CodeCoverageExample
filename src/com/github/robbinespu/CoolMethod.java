94b441959c6ee6b6185aaabaeaec4af137e1a4d6
Robbi Nespu <robbinespu@gmail.com>
2021-05-15 05:36:28 +0800

    refactor var "result" as private static

--- src/com/github/robbinespu/CoolMethod.java
+++ src/com/github/robbinespu/CoolMethod.java
@@ -1,8 +1,8 @@
 package com.github.robbinespu;
 
 public class CoolMethod {
+    private static int result;
     public static int methodBar(int p1, int p2) {
-        int result = 0;
         if (p1 > 0 && p2 > 0) {
             result = p1;
         }
